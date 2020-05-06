7c4173239f14b019abcaebf129ed45365024dc52
hudaokeji <github@hudaokeji.com>
2020-05-06 13:20:32 +0900

    update

--- src-bak/loaders/obj2/worker/parallel/OBJLoader2JsmWorker.js
+++
@@ -1,7 +0,0 @@
-define([
-    '../../OBJLoader2Parser.js',
-    './WorkerRunner.js'
-], function (a, b) {
-    'use strict';
-    new b.WorkerRunner(new b.DefaultWorkerPayloadHandler(new a.OBJLoader2Parser()));
-});
--- src-bak/objects/ReflectorRTT.js
+++
@@ -1,9 +0,0 @@
-define(['../objects/Reflector.js'], function (a) {
-    'use strict';
-    var ReflectorRTT = function (geometry, options) {
-        a.Reflector.call(this, geometry, options);
-        this.geometry.setDrawRange(0, 0);
-    };
-    ReflectorRTT.prototype = Object.create(a.Reflector.prototype);
-    return ReflectorRTT;
-});
